b49313720258441e0bc757da30e731344d035710
Mike <mike.eirich@gmx.de>
2021-11-27 15:21:15 +0100

    changing asset paths

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
   // Todo: change localhost to the dockername
-  API_URL: "/kniffel/api"
+  // for local docker testing
+  API_URL: "http://localhost:8082/kniffel/api",
+  // for server deployment
+  //API_URL: "kniffel/api"
 };
