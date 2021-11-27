bf33ad5445d3b935ba078ee71dabd1351b335cf3
Mike <mike.eirich@gmx.de>
2021-11-27 16:07:44 +0100

    removed todo marker

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // Todo: change localhost to the dockername
   // for local docker testing
   // API_URL: "http://localhost:8082/kniffel/api",
   // for server deployment
