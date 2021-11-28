fce957c714f2460afa0bc80a75fc438f0a38e98b
Mike <mike.eirich@gmx.de>
2021-11-28 17:01:26 +0100

    updated production API_URL

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   // for local docker testing
   // API_URL: "http://localhost:8082/kniffel/api",
   // for server deployment
-  API_URL: "/kniffel/api"
+  API_URL: "./kniffel/api"
 };
