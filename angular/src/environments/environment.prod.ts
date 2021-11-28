8cbd9172bc60d9a1ff3acf3cc6c9606281a59f0a
Mike <mike.eirich@gmx.de>
2021-11-28 17:38:27 +0100

    removed old outcommented API_URL

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  // for local docker testing
-  // API_URL: "http://localhost:8082/kniffel/api",
-  // for server deployment
   API_URL: "./api"
 };
