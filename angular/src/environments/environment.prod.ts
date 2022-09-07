5f4d7dac934cdd0192be3c2329ea08a7d1fa8d57
Mike <mike.eirich@gmx.de>
2022-09-07 20:43:12 +0200

    Updated production env variables

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  REST_API_URL: 'http://localhost:8083/api/v1',
-  WEBSOCKET_URL: 'ws://localhost:8083/ws/',
+  REST_API_URL: 'https://manakee.de/api/v1',
+  WEBSOCKET_URL: 'ws://manakee.de/ws/',
 };
