79993aa0a715b72b433c7d6881d06220b44301b1
Mike <mike.eirich@gmx.de>
2022-09-07 20:50:37 +0200

    Added kniffel to API and WS path

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  REST_API_URL: 'https://manakee.de/api/v1',
-  WEBSOCKET_URL: 'ws://manakee.de/ws/',
+  REST_API_URL: 'https://manakee.de/kniffel/api/v1',
+  WEBSOCKET_URL: 'ws://manakee.de/kniffel/ws/',
 };
